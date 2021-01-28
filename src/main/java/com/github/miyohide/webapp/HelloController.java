59dc0418e903184fbac1ca5c3961678c08f80d60
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-28 21:09:07 +0900

    display Cloud Foundry instance GUID

--- src/main/java/com/github/miyohide/webapp/HelloController.java
+++ src/main/java/com/github/miyohide/webapp/HelloController.java
@@ -5,11 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Optional;
+
 @Controller
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
-        model.addAttribute("message", "Hello Spring Boot!");
+        Optional<String> guid = Optional.ofNullable(System.getenv("CF_INSTANCE_GUID"));
+        model.addAttribute("instance_guid", guid.orElse("CF_INSTANCE_GUID is null"));
         return "index";
     }
 }
