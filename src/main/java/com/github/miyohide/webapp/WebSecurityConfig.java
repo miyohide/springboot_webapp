5c07001ab62ec51e7b51dbb3dacdbd081caffb9c
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-30 17:24:45 +0900

    config Spring Security

---
+++ src/main/java/com/github/miyohide/webapp/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.github.miyohide.webapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.debug(false)
+                .ignoring()
+                .antMatchers("/images/**", "/js/**", "/css/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/");
+    }
+}
