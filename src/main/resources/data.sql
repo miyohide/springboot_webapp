1b7ca8d324de05b7307c2569263c16e74d5ef512
Hidenori Miyoshi <miyohide@gmail.com>
2021-01-31 18:29:42 +0900

    use data.sql for Database initialize.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (email, password, roles) VALUES ('test@example.com', '{bcrypt}$2a$08$IJlH.0iYk5A4IBXp854x..fYyCaikIbTiBqtlCyJoQGp/bVAIDXEG', 'ROLE_USER,ROLE_ADMIN');
+INSERT INTO user (email, password, roles) VALUES ('test2@example.com', '{bcrypt}$2a$08$MZ5rf.USNslk811NnG6MQe9N.Jq2X0Uybf/7DVC79I7L4G3YpVxnu', 'ROLE_USER,ROLE_ADMIN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,5 +7,3 @@ CREATE TABLE user (
     PRIMARY KEY (id),
     UNIQUE KEY (email)
 );
-
-INSERT INTO user (email, password, roles) VALUES ('test@example.com', '{bcrypt}$2a$10$uhuqnvtjTayBhSjs7ezeB.2DG5GlIERAawzRoCROyTxWpzwKy7T.e', 'ROLE_USER,ROLE_ADMIN');
